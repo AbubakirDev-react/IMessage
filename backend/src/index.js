df373e8499f7287cfa9111c0f36324bc1a0391cf
AbubakirDev-react <2000ak47123456@gmail.com>
2026-06-13 08:50:09 +0500

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+console.log('DB-URL:', process.env.DB_URL);
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
